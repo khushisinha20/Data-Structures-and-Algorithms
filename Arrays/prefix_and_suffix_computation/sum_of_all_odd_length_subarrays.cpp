f34a4e7b5208e47b4bbb064cdf48804d9047f2df
Khushi <khushisinha4786@gmail.com>
2022-01-06 00:02:31 +0530

    Added Sum of all odd length subarrays

---
+++ Arrays/prefix_and_suffix_computation/sum_of_all_odd_length_subarrays.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int result = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; ++i) {
+            int end = i + 1;
+            int start = n - i;
+            int total = start * end;
+            int odd = total / 2;
+            if (total % 2 == 1)
+                ++odd;
+            result += odd * arr[i];
+        }
+        return result;
+    }
+};

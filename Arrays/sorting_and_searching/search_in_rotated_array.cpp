fbd599a42a7ab210cb3bdd3df5606fe9661396e8
Khushi <khushisinha4786@gmail.com>
2022-01-18 23:19:48 +0530

    Added search in a rotated array

---
+++ Arrays/sorting_and_searching/search_in_rotated_array.cpp
@@ -0,0 +1,32 @@
+//leetcode.com/problems/search-in-rotated-sorted-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[low]) {
+                if (target <= nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};

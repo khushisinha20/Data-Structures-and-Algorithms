ac715e22e952271052c1fcd8509dbdcbe73215fd
Khushi <khushisinha4786@gmail.com>
2022-01-21 16:34:11 +0530

    Added Search Insert Position

---
+++ Arrays/sorting_and_searching/search_insert_position.cpp
@@ -0,0 +1,24 @@
+//leetcode.com/problems/search-insert-position/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};

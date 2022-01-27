e6017c759c66be7bc91db8d5fea9e48dbb4d494d
Khushi <khushisinha4786@gmail.com>
2022-01-27 11:59:30 +0530

    Added Valid Anagram

---
+++ Strings/valid_anagram.cpp
@@ -0,0 +1,22 @@
+//leetcode.com/problems/valid-anagram/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        int freq[26] = {0};
+        for (int i = 0; i < s.length(); ++i) {
+            ++freq[s[i] - 'a'];
+            --freq[t[i] - 'a'];
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};

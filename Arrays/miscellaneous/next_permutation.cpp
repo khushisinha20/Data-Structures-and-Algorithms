22367964634ec75f9fc7833ec852d084d77c308b
Khushi <khushisinha4786@gmail.com>
2022-02-04 22:14:51 +0530

    Added next permutation

---
+++ Arrays/miscellaneous/next_permutation.cpp
@@ -0,0 +1,33 @@
+//leetcode.com/problems/next-permutation/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() == 1)
+            return;
+        int idx1 = INT_MAX;
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                idx1 = i;
+                break;
+            }
+        }
+        
+        if (idx1 == INT_MAX) {
+            reverse(nums.begin(), nums.end());
+        } else {
+            int idx2 = 0;
+            for (int i = nums.size() - 1; i >= 0; --i) {
+                if (nums[i] > nums[idx1]) {
+                    idx2 = i;
+                    break;
+                }
+            }
+            swap(nums[idx1], nums[idx2]);
+            reverse(nums.begin() + idx1 + 1, nums.end());
+        }
+    }
+};

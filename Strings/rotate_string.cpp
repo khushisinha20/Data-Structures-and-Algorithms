f936c52e8008e0373bba41c0144303be188b00bc
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-02-16 01:56:43 +0530

    Added rotate string

---
+++ Strings/rotate_string.cpp
@@ -0,0 +1,13 @@
+//leetcode.com/problems/rotate-string/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length())
+            return false;
+        return ((s + s).find(goal) != string :: npos);
+    }
+};

6ea04e2ba42d231f51b12b00955ce2d12231e2cb
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-02-18 22:06:06 +0530

    Added Minimum Number of Steps to Make Two Strings Anagram

---
+++ Strings/min_steps_to_make_2_str_anagram.cpp
@@ -0,0 +1,25 @@
+//leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int min_steps = 0;
+        vector<int> freq(26, 0);
+        if (s == t)
+            return 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ++freq[s[i] - 'a'];
+            --freq[t[i] - 'a'];
+        }
+        for (auto i: freq) {
+            if (i > 0) 
+                min_steps += i;
+        }
+        return min_steps;
+    }
+};

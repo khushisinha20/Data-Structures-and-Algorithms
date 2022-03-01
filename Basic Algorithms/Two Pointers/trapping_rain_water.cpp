c1a3f9c09104323043d90a8b4f96269886aeb24e
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-03-01 18:12:17 +0530

    Added Trapping Rain Water

---
+++ Basic Algorithms/Two Pointers/trapping_rain_water.cpp
@@ -0,0 +1,35 @@
+//leetcode.com/problems/trapping-rain-water/
+
+#include <bits/stdc++.h>
+using namepace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.size() < 3)
+            return 0;
+        int maxLeft = height[0];
+        int maxRight = height[height.size() - 1];
+        int trappedWater = 0;
+        int left = 1;
+        int right = height.size() - 2;
+        while (left <= right) {
+            if (maxLeft < maxRight) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    trappedWater += (maxLeft - height[left]);
+                }
+                ++left;
+            } else {
+                if (height[right] > maxRight) {
+                    maxRight = height[right];
+                } else {
+                    trappedWater += (maxRight - height[right]);
+                }
+                --right;
+            }
+        }
+        return trappedWater;
+    }
+};

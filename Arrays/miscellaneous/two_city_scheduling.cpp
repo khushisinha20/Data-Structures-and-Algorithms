d8d1883c541b6288b3aa25a091c9ebf3b694de29
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-03-26 01:26:46 +0530

    Added Two City Scheduling

---
+++ Arrays/miscellaneous/two_city_scheduling.cpp
@@ -0,0 +1,22 @@
+//leetcode.com/problems/two-city-scheduling/submissions/
+
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+bool comparator(vector<int>& a, vector<int>& b) {
+    return (a[0] - a[1]) < (b[0] - b[1]);
+}
+    
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), comparator);
+        int min_cost = 0;
+        for (int i = 0; i < costs.size() / 2; ++i) 
+            min_cost += costs[i][0];
+        for (int i = costs.size() / 2; i < costs.size(); ++i) 
+            min_cost += costs[i][1];
+        return min_cost;
+    }
+};

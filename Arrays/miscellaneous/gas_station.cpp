81c70ec7614bfc9af62081477c8a958a550099a9
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-04-19 03:11:10 +0530

    Added Gas Station

--- Arrays/miscellaneous/gas_station.cpp
+++ Arrays/miscellaneous/gas_station.cpp
@@ -7,17 +7,21 @@ using namespace std;
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int surplus = 0;
-        int total_surplus = 0;
+        int total_amount_of_gas = 0;
+        int total_cost = 0;
+        total_amount_of_gas = accumulate(gas.begin(), gas.end(), total_amount_of_gas);
+        total_cost = accumulate(cost.begin(), cost.end(), total_cost);
+        int current_gas = 0;
         int start = 0;
+        if (total_amount_of_gas < total_cost)
+            return -1;
         for (int i = 0; i < gas.size(); ++i) {
-            total_surplus += (gas[i] - cost[i]);
-            surplus += (gas[i] - cost[i]);
-            if (surplus < 0) {
-                surplus = 0;
+            current_gas += (gas[i] - cost[i]);
+            if (current_gas < 0) {
                 start = i + 1;
+                current_gas = 0;
             }
         }
-        return (total_surplus < 0) ? -1 : start;
+        return start;
     }
 };

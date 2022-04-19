866ce996a1405e969d209f418025f722b1d75687
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-04-19 19:20:26 +0530

    Added Insert Intervals

--- Arrays/miscellaneous/insert_intervals.cpp
+++ Arrays/miscellaneous/insert_intervals.cpp
@@ -6,28 +6,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > insert(vector<vector<int> >& intervals, vector<int>& newInterval) {
-        vector<vector<int> > res;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
         int i = 0;
-        while (i < intervals.size() && intervals[i][0] <= newInterval[0]) {
-            res.push_back({intervals[i][0], intervals[i][1]});
-            ++i;
+        //Phase 1: Does not overlap before merging
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) 
+            result.push_back(intervals[i++]);
+        
+        //Phase 2: Overlaps
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i++][1]);
         }
-        if (res.size() == 0 || res.back()[1] < newInterval[0]) {
-            res.push_back({newInterval[0], newInterval[1]});
-        } else {
-            res.back()[0] = min(newInterval[0], res.back()[0]);
-            res.back()[1] = max(newInterval[1], res.back()[1]);
-        }
-        while (i < intervals.size() && res.back()[1] >= intervals[i][0]) {
-            res.back()[0] = min(intervals[i][0], res.back()[0]);
-            res.back()[1] = max(intervals[i][1], res.back()[1]);
-            ++i;
-        }
-        while (i < intervals.size()) {
-            res.push_back(intervals[i]);
-            ++i;
-        }
-        return res;
+        result.push_back(newInterval);
+        
+        //Phase 3: Does not overlap after merging
+        while (i < intervals.size()) 
+            result.push_back(intervals[i++]);
+        
+        return result;
     }
 };

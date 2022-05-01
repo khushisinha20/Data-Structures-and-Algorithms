cb9f1dd00cab42e79357c74ffbc785e88083385f
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-05-01 23:59:52 +0530

    Added Next Permutation

--- Arrays/miscellaneous/next_permutation.cpp
+++ Arrays/miscellaneous/next_permutation.cpp
@@ -6,28 +6,25 @@ using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if (nums.size() == 1)
-            return;
-        int idx1 = INT_MAX;
+        int less_than_next = -1;
+        int idx = -1;
         for (int i = nums.size() - 2; i >= 0; --i) {
             if (nums[i] < nums[i + 1]) {
-                idx1 = i;
+                less_than_next = i;
                 break;
             }
         }
-        
-        if (idx1 == INT_MAX) {
+        if (less_than_next == -1) {
             reverse(nums.begin(), nums.end());
         } else {
-            int idx2 = 0;
             for (int i = nums.size() - 1; i >= 0; --i) {
-                if (nums[i] > nums[idx1]) {
-                    idx2 = i;
+                if (nums[i] > nums[less_than_next]) {
+                    idx = i;
                     break;
                 }
             }
-            swap(nums[idx1], nums[idx2]);
-            reverse(nums.begin() + idx1 + 1, nums.end());
+            swap(nums[less_than_next], nums[idx]);
+            reverse(nums.begin() + less_than_next + 1, nums.end());
         }
     }
 };

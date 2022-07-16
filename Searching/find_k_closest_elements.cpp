9f3b8fee9904c18cbb3e684d13b22a38666f005e
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-07-17 02:15:56 +0530

    Added Find K Closest Elements

---
+++ Searching/find_k_closest_elements.cpp
@@ -0,0 +1,23 @@
+//leetcode.com/problems/find-k-closest-elements/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int low = 0;
+        int high = arr.size() - k;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        vector<int> res;
+        for (int i = low; i < low + k; ++i)
+            res.push_back(arr[i]);
+        return res;
+    }
+};

94bcc2325034ddd2999ea934e7f978b82bcc79c9
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-07-30 14:22:48 +0530

    Added Valid stack sequences

---
+++ Stack/valid_stack_sequences.cpp
@@ -0,0 +1,20 @@
+//leetcode.com/problems/validate-stack-sequences/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int i = 0;
+        int j = 0;
+        for (auto val: pushed) {
+            pushed[i++] = val;
+            while (i > 0 && pushed[i - 1] == popped[j]) {
+                --i;
+                ++j;
+            }
+        }
+        return i == 0;
+    }
+};

1babebc4a3dae2ff8085c40256ca69635fa1ff57
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-08-01 23:09:07 +0530

    Added Iterative postorder traversal using two stacks

---
+++ Trees/Binary Trees/dfs/postorder_traversal_iterative_two_stacks.cpp
@@ -0,0 +1,45 @@
+//leetcode.com/problems/binary-tree-postorder-traversal/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root)
+            return {};
+        
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        vector<int> postorder;
+        s1.push(root);
+        
+        while (!s1.empty()) {
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            
+            if (root -> left)
+                s1.push(root -> left);
+            
+            if (root -> right)
+                s1.push(root -> right);
+        }
+        
+        while (!s2.empty()) {
+            postorder.push_back(s2.top() -> val);
+            s2.pop();
+        }
+        
+        return postorder;
+    }
+};

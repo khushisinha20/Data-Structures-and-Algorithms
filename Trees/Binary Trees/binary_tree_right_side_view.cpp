f7a8da187a5bc72d184ae121cb74681e13fed283
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-08-08 18:17:52 +0530

    Added Binary Tree Right Side View

---
+++ Trees/Binary Trees/binary_tree_right_side_view.cpp
@@ -0,0 +1,38 @@
+//leetcode.com/problems/binary-tree-right-side-view/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<int> res;
+        if (!root)
+            return res;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (i == n - 1)
+                    res.push_back(node -> val);
+                if (node -> left)
+                    q.push(node -> left);
+                if (node -> right)
+                    q.push(node -> right);
+            }
+        }
+        return res;
+    }
+};

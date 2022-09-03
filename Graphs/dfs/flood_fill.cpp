076cf07f87082920e4ed6e51e88f1faf13bb9c4d
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-09-04 01:12:07 +0530

    Added Flood Fill

---
+++ Graphs/dfs/flood_fill.cpp
@@ -0,0 +1,30 @@
+//leetcode.com/problems/flood-fill/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int source_pixel_color) {
+        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size())
+            return;
+        
+        if (image[sr][sc] != source_pixel_color)
+            return;
+        
+        if (image[sr][sc] == color)
+            return;
+        
+        image[sr][sc] = color;
+        dfs(image, sr + 1, sc, color, source_pixel_color);
+        dfs(image, sr - 1, sc, color, source_pixel_color);
+        dfs(image, sr, sc + 1, color, source_pixel_color);
+        dfs(image, sr, sc - 1, color, source_pixel_color);
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int source_pixel_color = image[sr][sc];
+        dfs(image, sr, sc, color, source_pixel_color);
+        return image;
+    }
+};

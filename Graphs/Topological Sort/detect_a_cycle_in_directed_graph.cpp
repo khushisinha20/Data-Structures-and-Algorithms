1bcf4d6d910cabaddcfbd50274cdb2976f789200
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-09-20 00:50:17 +0530

    Added Detect cycle in a directed graph

---
+++ Graphs/Topological Sort/detect_a_cycle_in_directed_graph.cpp
@@ -0,0 +1,48 @@
+//practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void fillIndegree(vector<int> adj[], vector<int>& indegree, int& V) {
+	    for (int i = 0; i < V; ++i) {
+	        for (auto adjacentVertex: adj[i]) {
+	            ++indegree[adjacentVertex];
+	        }
+	    }
+	}
+	
+	void storeNodesHavingZeroIndegree(queue<int>& zeroIndegreeNodes, vector<int>& indegree) {
+	    for (int vertex = 0; vertex < indegree.size(); ++vertex)
+	        if (indegree[vertex] == 0)
+	            zeroIndegreeNodes.push(vertex);
+	}
+	
+	void bfs(queue<int>& zeroIndegreeNodes, vector<int>& indegree, vector<int> adj[], vector<int>& topologicalSortedNodes) {
+	    while (!zeroIndegreeNodes.empty()) {
+	        int currentNode = zeroIndegreeNodes.front();
+	        zeroIndegreeNodes.pop();
+	        topologicalSortedNodes.push_back(currentNode);
+	        for (auto adjacentVertex: adj[currentNode]) {
+	            --indegree[adjacentVertex];
+	            if (indegree[adjacentVertex] == 0)
+	                zeroIndegreeNodes.push(adjacentVertex);
+	        }
+	    }
+	}
+	
+	vector<int> topoSort(int V, vector<int> adj[]) {
+	    vector<int> indegree(V, 0);
+	    fillIndegree(adj, indegree, V);
+	    queue<int> zeroIndegreeNodes;
+	    storeNodesHavingZeroIndegree(zeroIndegreeNodes, indegree);
+	    vector<int> topologicalSortedNodes;
+	    bfs(zeroIndegreeNodes, indegree, adj, topologicalSortedNodes);
+	    return topologicalSortedNodes;
+	}
+  
+    bool isCyclic(int V, vector<int> adj[]) {
+        return topoSort(V, adj).size() != V;
+    }
+};

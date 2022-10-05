89ca641792c99ac65c54a38605fec658c4e6387f
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-10-05 22:41:46 +0530

    Added Delete Leaves With a Given Value

---
+++ Trees/Binary Trees/delete_leaves_with_given_value.cpp
@@ -0,0 +1,24 @@
+//leetcode.com/problems/delete-leaves-with-a-given-value/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root)
+            return nullptr;
+        root -> left = removeLeafNodes(root -> left, target);
+        root -> right = removeLeafNodes(root -> right, target);
+        return !root -> right and !root -> left and root -> val == target ? nullptr : root;
+    }
+};

91c03a84069867e8861631726230de4472cd284b
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-10-19 22:55:33 +0530

    Added Find the Town Judge

---
+++ Graphs/find_the_town_judge.cpp
@@ -0,0 +1,21 @@
+//leetcode.com/problems/find-the-town-judge/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n + 1, 0);
+        vector<int> outdegree(n + 1, 0);
+        for (int i = 0; i < trust.size(); ++i) {
+            ++indegree[trust[i][1]];
+            ++outdegree[trust[i][0]];
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0)
+                return i;
+        }
+        return -1;
+    }
+};

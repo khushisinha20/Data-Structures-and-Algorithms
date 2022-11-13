d0dd20d41c402678cc298ed7a2ef6147a5ebefdb
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-11-13 21:17:53 +0530

    Added Subsets II

--- Recursion and Backtracking/subsets_II.cpp
+++ Recursion and Backtracking/subsets_II.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 class Solution {
 public:
-    void generateSubsets(vector<vector<int>>& result, vector<int>& temp, vector<int>& nums, int start) {
-        result.push_back(temp);
-        
-        for (int i = start; i < nums.size(); ++i) {
-            if (i > start && nums[i] == nums[i - 1])
-                continue;
-            
-            temp.push_back(nums[i]);
-            generateSubsets(result, temp, nums, i + 1);
-            temp.pop_back();
+    void backtrack(vector<int>& nums, int currentPosition, vector<int>& currentSubset, vector<vector<int>>& result) {
+        if (currentPosition == nums.size()) {
+            result.push_back(currentSubset);
+            return;
         }
+        
+        currentSubset.push_back(nums[currentPosition]);
+        backtrack(nums, currentPosition + 1, currentSubset, result);
+        currentSubset.pop_back();
+        while (currentPosition + 1 < nums.size() && nums[currentPosition] == nums[currentPosition + 1])
+            ++currentPosition;
+        backtrack(nums, currentPosition + 1, currentSubset, result);
     }
     
-    vector<vector<int>> subsetsWithDup(vector<int> nums) {
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        vector<int> temp;
-        generateSubsets(result, temp, nums, 0);
+        vector<int> currentSubset;
+        backtrack(nums, 0, currentSubset, result);
         return result;
     }
 };

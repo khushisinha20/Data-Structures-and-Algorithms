e284755ddd97c33e2a75304d33b31740d5bbf2bc
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-11-14 17:14:57 +0530

    Added Combination Sum II

--- Recursion and Backtracking/combination_sum_||.cpp
+++ Recursion and Backtracking/combination_sum_||.cpp
@@ -5,28 +5,28 @@ using namespace std;
 
 class Solution {
 public:
-    void generateCombinations(vector<vector<int>>& res, vector<int>& temp, vector<int>& candidates, int target, int start) {
-        if (target == 0) {
-            res.push_back(temp);
+    void backtrack(vector<int>& candidates, int target,  vector<vector<int>>& combinations, vector<int>& currentCombination, int index) {
+       if (target == 0) {
+            combinations.push_back(currentCombination);
             return;
         }
         
-        for (int i = start; i < candidates.size(); ++i) {
-            if (i > start && candidates[i - 1] == candidates[i])
+        for (int i = index; i < candidates.size(); ++i) {
+            if (i > index && candidates[i - 1] == candidates[i])
                 continue;
             if (candidates[i] > target)
                 break;
-            temp.push_back(candidates[i]);
-            generateCombinations(res, temp, candidates, target - candidates[i], i + 1);
-            temp.pop_back();
+            currentCombination.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], combinations, currentCombination, i + 1);
+            currentCombination.pop_back();
         }
     }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> temp;
         sort(candidates.begin(), candidates.end());
-        generateCombinations(res, temp, candidates, target, 0);
-        return res;
+        vector<vector<int>> combinations;
+        vector<int> currentCombination;
+        backtrack(candidates, target, combinations, currentCombination, 0);
+        return combinations;
     }
 };

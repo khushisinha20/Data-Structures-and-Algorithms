4fa0666dd8c09ad9f00dab821b2a78d5f71593b3
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-11-19 00:17:54 +0530

    Added Ugly Number

---
+++ Math/ugly_number.cpp
@@ -0,0 +1,21 @@
+//leetcode.com/problems/ugly-number/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        while (n > 1) {
+            if (n % 2 == 0)
+                n /= 2;
+            else if (n % 3 == 0)
+                n /= 3;
+            else if (n % 5 == 0)
+                n /= 5;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+};

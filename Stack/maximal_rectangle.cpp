4a951d3ee211c925bb318d7159b2ae89972c679b
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-12-25 09:46:59 +0530

    Added Maximal Rectangle

--- Stack/maximal_rectangle.cpp
+++ Stack/maximal_rectangle.cpp
@@ -5,75 +5,72 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> nearestSmallerLeft(vector<int>& histogram, vector<int>& arr) {
-        stack<pair<int, int>> s;
-        for (int i = 0; i < histogram.size(); ++i) {
-            if (s.empty())
-                arr[i] = -1;
-            else if (!s.empty() && s.top().first < histogram[i])
-                arr[i] = s.top().second;
-            else if (!s.empty() && s.top().first >= histogram[i]) {
-                while (!s.empty() && s.top().first >= histogram[i])
-                    s.pop();
-                if (s.empty())
-                    arr[i] = -1;
+    vector<int> nearestSmallerToLeft(vector<int>& heights) {
+        vector<int> nsl(heights.size());
+        stack<pair<int, int>> previousHeights;
+        
+        for (int i = 0; i < heights.size(); ++i) {
+            if (!previousHeights.empty() && previousHeights.top().first < heights[i])
+                nsl[i] = previousHeights.top().second;
+            else {
+                while (!previousHeights.empty() && previousHeights.top().first >= heights[i])
+                    previousHeights.pop();
+                if (previousHeights.empty())
+                    nsl[i] = -1;
                 else
-                    arr[i] = s.top().second;
+                    nsl[i] = previousHeights.top().second;
             }
-            s.push({histogram[i], i});
+            previousHeights.push({heights[i], i});
         }
-        return arr;
+        return nsl;
     }
     
-    vector<int> nearestSmallerRight(vector<int>& histogram, vector<int>& arr) {
-        stack<pair<int, int>> s;
-        for (int i = histogram.size() - 1; i >= 0; --i) {
-            if (s.empty())
-                arr[i] = histogram.size();
-            else if (!s.empty() && s.top().first < histogram[i])
-                arr[i] = s.top().second;
-            else if (!s.empty() && s.top().first >= histogram[i]) {
-                while (!s.empty() && s.top().first >= histogram[i])
-                    s.pop();
-                if (s.empty())
-                    arr[i] = histogram.size();
+    vector<int> nearestSmallerToRight(vector<int>& heights) {
+        vector<int> nsr(heights.size());
+        stack<pair<int, int>> nextHeights;
+        
+        for (int i = heights.size() - 1; i >= 0; --i) {
+            if (!nextHeights.empty() && nextHeights.top().first < heights[i])
+                nsr[i] = nextHeights.top().second;
+            else {
+                while (!nextHeights.empty() && nextHeights.top().first >= heights[i])
+                    nextHeights.pop();
+                if (nextHeights.empty())
+                    nsr[i] = heights.size();
                 else
-                    arr[i] = s.top().second;
+                    nsr[i] = nextHeights.top().second;
             }
-            s.push({histogram[i], i});
+            nextHeights.push({heights[i], i});
         }
-        return arr;
+        return nsr;
     }
     
-    int maximumAreaInHistogram(vector<int>& histogram) {
-        vector<int> nearest_smaller_left(histogram.size());
-        vector<int> nearest_smaller_right(histogram.size());
-        nearestSmallerLeft(histogram, nearest_smaller_left);
-        nearestSmallerRight(histogram, nearest_smaller_right);
-        int max_area = 0;
-        for (int i = 0; i < histogram.size(); ++i) {
-            int width = nearest_smaller_right[i] - nearest_smaller_left[i] - 1;
-            max_area = max(max_area, histogram[i] * width);
+    int maximumAreaRectangle(vector<int>& heights) {
+        vector<int> nsl = nearestSmallerToLeft(heights);
+        vector<int> nsr = nearestSmallerToRight(heights);
+        int maxAreaRectangle = 0;
+        for (int i = 0; i < heights.size(); ++i) {
+            int width = nsr[i] - nsl[i] - 1;
+            maxAreaRectangle = max(maxAreaRectangle, width * heights[i]);
         }
-        return max_area;
+        return maxAreaRectangle;
+    }
+    
+    void updateHistogram(vector<int>& histogram, vector<vector<char>>& matrix, int row) {
+        for (int i = 0; i < matrix[row].size(); ++i) 
+            if (matrix[row][i] == '0')
+                histogram[i] = 0;
+            else
+                ++histogram[i];
     }
     
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<int> histogram;
-        for (int col = 0; col < matrix[0].size(); ++col) 
-            histogram.push_back(matrix[0][col] - '0');
-        
-        int maximal_rectangle = maximumAreaInHistogram(histogram);
-        for (int row = 1; row < matrix.size(); ++row) {
-            for (int col = 0; col < matrix[0].size(); ++col) {
-                if (matrix[row][col] != '0')
-                    histogram[col] += (matrix[row][col] - '0');
-                else
-                    histogram[col] = 0;
-            }
-            maximal_rectangle = max(maximal_rectangle, maximumAreaInHistogram(histogram));
+        vector<int> histogram(matrix[0].size(), 0);
+        int maxArea = 0;
+        for (int row = 0; row < matrix.size(); ++row) {
+            updateHistogram(histogram, matrix, row);
+            maxArea = max(maxArea, maximumAreaRectangle(histogram)); 
         }
-        
-        return maximal_rectangle;
+        return maxArea;
     }
 };

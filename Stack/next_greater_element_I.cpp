4b8718928153fb082b802c53d453bd4e8c8f92ea
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2022-12-25 06:47:44 +0530

    Added Next Greater Element I

--- Stack/next_greater_element_I.cpp
+++ Stack/next_greater_element_I.cpp
@@ -5,31 +5,31 @@ using namespace std;
 
 class Solution {
 public:
-    void findNextGreater(vector<int>& nums2, unordered_map<int, int>& mp) {
-        stack<int> s;
-        for (int i = nums2.size() - 1; i >= 0; --i) {
-            if (s.empty()) 
-                mp[nums2[i]] = -1;   
-            else if (!s.empty() && nums2[i] < s.top()) 
-                mp[nums2[i]] = s.top();
-            else if (!s.empty() && nums2[i] > s.top()) {
-                while (!s.empty() && nums2[i] >= s.top())
-                    s.pop();
-                if (s.empty())
-                    mp[nums2[i]] = -1;
-                else
-                    mp[nums2[i]] = s.top();
-            }
-            s.push(nums2[i]);
-        }
+    void extractNextGreaterForSubset(unordered_map<int, int>& nextGreater, vector<int>& ans, vector<int>& nums1) {
+        for (auto num: nums1)
+            ans.push_back(nextGreater[num]);
     }
     
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> next_greater_elements;
-        vector<int> res;
-        findNextGreater(nums2, next_greater_elements);
-        for (auto num: nums1)
-            res.push_back(next_greater_elements[num]);
-        return res;
+        unordered_map<int, int> nextGreater;
+        stack<int> elements;
+        
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            if (!elements.empty() && elements.top() > nums2[i])
+                nextGreater[nums2[i]] = elements.top();
+            else {
+                while (!elements.empty() && elements.top() <= nums2[i])
+                    elements.pop();
+                if (elements.empty())
+                    nextGreater[nums2[i]] = -1;
+                if (!elements.empty() && elements.top() > nums2[i])
+                    nextGreater[nums2[i]] = elements.top();
+            }
+            elements.push(nums2[i]);
+        }
+        
+        vector<int> ans;
+        extractNextGreaterForSubset(nextGreater, ans, nums1);
+        return ans;
     }
 };

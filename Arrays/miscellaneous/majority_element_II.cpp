ff878e574418827f13faf6694ce01f4467a5f541
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-02-12 17:44:04 +0530

    Added Majority Element II

--- Arrays/miscellaneous/majority_element_II.cpp
+++ Arrays/miscellaneous/majority_element_II.cpp
@@ -6,39 +6,46 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int num1 = INT_MAX;
-        int num2 = INT_MAX;
-        int count_of_num1 = 0;
-        int count_of_num2 = 0;
-        for (auto element: nums) {
-            if (element  == num1)
-                ++count_of_num1;
-            else if (element == num2)
-                ++count_of_num2;
-            else if (count_of_num1 == 0) {
-                num1 = element;
-                count_of_num1 = 1;
-            } else if (count_of_num2 == 0) {
-                num2 = element;
-                count_of_num2 = 1;
+        int count1 = 0;
+        int count2 = 0;
+        
+        int num1 = INT_MIN;
+        int num2 = INT_MIN;
+        
+        for (auto num: nums) {
+            if (num == num1)
+                ++count1;
+            else if (num == num2)
+                ++count2;
+            else if (count1 == 0) {
+                num1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                num2 = num;
+                count2 = 1;
             } else {
-                --count_of_num1;
-                --count_of_num2;
+                --count1;
+                --count2;
             }
         }
-        vector<int> res;
-        int freq_of_num1 = 0;
-        int freq_of_num2 = 0;
-        for (auto element: nums) {
-            if (element == num1)
-                ++freq_of_num1;
-            if (element == num2)
-                ++freq_of_num2;
+        
+        vector<int> majority;
+        count1 = 0;
+        count2 = 0;
+        
+        for (auto num: nums) {
+            if (num == num1)
+                ++count1;
+            if (num == num2)
+                ++count2;
         }
-        if (freq_of_num1 > nums.size() / 3)
-            res.push_back(num1);
-        if (freq_of_num2 > nums.size() / 3)
-            res.push_back(num2);
-        return res;
+        
+        if (count1 > nums.size() / 3)
+             majority.push_back(num1);
+        
+        if (count2 > nums.size() / 3)
+             majority.push_back(num2);
+        
+        return majority;
     }
 };

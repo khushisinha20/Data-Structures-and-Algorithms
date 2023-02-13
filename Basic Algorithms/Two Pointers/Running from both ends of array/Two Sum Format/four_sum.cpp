6edcebe2cafa00e27f48d25278f782d23d84daaf
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-02-13 23:15:42 +0530

    Added 4 Sum

--- Basic Algorithms/Two Pointers/Running from both ends of array/Two Sum Format/four_sum.cpp
+++ Basic Algorithms/Two Pointers/Running from both ends of array/Two Sum Format/four_sum.cpp
@@ -1,7 +1,6 @@
 //leetcode.com/problems/4sum
 
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
 class Solution {
@@ -9,34 +8,38 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         if (nums.size() < 4)
             return {};
+        
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
+        vector<vector<int>> result;
+        
         for (int i = 0; i < nums.size() - 3; ++i) {
-            if (i > 0 && (nums[i] == nums[i - 1])) 
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             for (int j = i + 1; j < nums.size() - 2; ++j) {
-                if (j > i + 1 && (nums[j] == nums[j - 1])) 
+                if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
-                int sum = target - (nums[i] + nums[j]);
+                
+                long long currentTarget = target - ((long long)nums[j] + (long long)nums[i]);
+                
                 int low = j + 1;
                 int high = nums.size() - 1;
+                
                 while (low < high) {
-                    if (nums[low] + nums[high] == sum) {
-                        vector<int> ans = {nums[i], nums[j], nums[low], nums[high]};
-                        res.push_back(ans);
+                    if (nums[low] + nums[high] == currentTarget) {
+                        result.push_back({nums[i], nums[j], nums[low], nums[high]});
                         while (low < high && nums[low] == nums[low + 1])
                             ++low;
                         while (low < high && nums[high] == nums[high - 1])
                             --high;
                         ++low;
                         --high;
-                    } else if (nums[low] + nums[high] < sum) 
+                    } else if (nums[low] + nums[high] < currentTarget)
                         ++low;
                     else
                         --high;
                 }
             }
         }
-        return res;
+        return result;
     }
 };

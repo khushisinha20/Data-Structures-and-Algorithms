5a8c6ec770105363f8dacb4f603d469136eeeaaa
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-06-22 20:00:26 +0530

    Added String to Int

--- Strings/string_to_int.cpp
+++ Strings/string_to_int.cpp
@@ -5,26 +5,34 @@ using namespace std;
 
 class Solution {
 public:
+    long long atoi(string& s, int index, int sign, long long result) {
+        if (sign * result >= INT_MAX)
+            return INT_MAX;
+        
+        if (sign * result <= INT_MIN)
+            return INT_MIN;
+        
+        if (index >= s.length() or !isdigit(s[index]))
+            return result * sign;
+        
+        result = atoi(s, index + 1, sign, result * 10 + (s[index] - '0'));
+        return result;
+    }
+    
     int myAtoi(string s) {
-        int sign = 1;
-        int base = 0;
         int i = 0;
-        while (s[i] == ' ')
+        int n = s.length();
+        int sign = 1;
+        
+        while (i < n and s[i] == ' ')
             ++i;
-        if (s[i] == '-' || s[i] == '+') {
-            sign = 1 - 2 * (s[i] == '-');
+        
+        if (s[i] == '-') {
+            sign *= -1;
             ++i;
-        }
-        while (s[i] >= '0' && s[i] <= '9') {
-            if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7)) {
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
-            base = 10 * base + (s[i] - '0');
+        } else if (s[i] == '+') 
             ++i;
-        }
-        return sign * base;
+        
+        return atoi(s, i, sign, 0);
     }
 };

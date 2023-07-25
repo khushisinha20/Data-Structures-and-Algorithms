437fb64e025397fbb157088e5405db5109f3eb7c
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-07-25 14:32:20 +0530

    Added Coin Change II

--- Dynamic Programming/Unbounded Knapsack/coint_change_ii.cpp
+++ Dynamic Programming/Unbounded Knapsack/coint_change_ii.cpp
@@ -5,23 +5,58 @@ using namespace std;
 
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1, 0));
+    int helper(vector<int>& coins, int amount, int index, vector<vector<int>>& dp) {
+        // If the amount becomes negative, we can't make any more combinations.
+        if (amount < 0)
+            return 0;
         
-        for (int i = 0; i < coins.size() + 1; ++i) {
-            for (int j = 0; j < amount + 1; ++j) {
-                if (j == 0)
-                    dp[i][j] = true;
-                else if (i == 0)
-                    dp[i][j] = false;
-                else {
-                    if (coins[i - 1] <= j)
-                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                    else
-                        dp[i][j] = dp[i - 1][j];
-                }
+        // If the amount becomes 0, we have found a valid combination.
+        if (amount == 0)
+            return 1;
+        
+        // If we have exhausted all the coins, but the amount is still greater than 0,
+        // there are no valid combinations.
+        if (index >= coins.size())
+            return 0;
+        
+        // If the result for the current index and amount is already calculated, return it.
+        if (dp[index][amount] != -1)
+            return dp[index][amount];
+        
+        // Try to use the current coin and/or skip to the next coin.
+        int takeCoin = helper(coins, amount - coins[index], index, dp);
+        int skipCoin = helper(coins, amount, index + 1, dp);
+        
+        // Store the result in the DP table and return the total number of combinations.
+        return dp[index][amount] = takeCoin + skipCoin;
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+        return helper(coins, amount, 0, dp);
+    }
+};
+
+/*
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= coins.size(); ++i)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= coins.size(); ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                int skipCoin = dp[i - 1][j];
+                int includeCoin = 0;
+                if (j - coins[i - 1] >= 0)
+                    includeCoin = dp[i][j - coins[i - 1]];
+                dp[i][j] = skipCoin + includeCoin;
             }
         }
+
         return dp[coins.size()][amount];
     }
 };
+*/

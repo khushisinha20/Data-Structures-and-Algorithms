fbbddd9faa6171c41a5ea540fba5e23d67747741
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-08-04 23:30:39 +0530

    Added Letter Combinations of a Phone Number

--- Recursion and Backtracking/letter_combinations_of_a_phone_number.cpp
+++ Recursion and Backtracking/letter_combinations_of_a_phone_number.cpp
@@ -5,26 +5,37 @@ using namespace std;
 
 class Solution {
 public:
+    void helper(int index, string& digits, string& currentCombination, unordered_map<char, string>& digitMap, vector<string>& result) {
+        if (index == digits.length()) {
+            result.push_back(currentCombination);
+            return;
+        }
+        
+        for (int i = 0; i < digitMap[digits[index]].size(); ++i) {
+            currentCombination.push_back(digitMap[digits[index]][i]);
+            helper(index + 1, digits, currentCombination, digitMap, result);
+            currentCombination.pop_back();
+        }
+    }
+    
     vector<string> letterCombinations(string digits) {
-        vector<string> letters = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        vector<string> res;
-        if (digits.size() == 0) 
+        if (digits.empty())
             return {};
-        if (digits.size() == 1) {
-            string str = letters[digits[0] - '0'];
-            vector<string> res(str.length());;
-            for (int i = 0; i < str.length(); ++i) 
-                res[i] = str[i];
-            return res;
-        }
-        char first_digit = digits[0];
-        string rest_of_string = digits.substr(1, digits.length() - 1);
-        vector<string> combinations = letterCombinations(rest_of_string);
-        for (auto i: letters[first_digit - '0']) {
-            for (auto j: combinations) {
-                res.push_back(i + j);
-            }
-        }
-        return res;
+        
+        unordered_map<char, string> digitMap;
+        digitMap['2'] = "abc";
+        digitMap['3'] = "def";
+        digitMap['4'] = "ghi";
+        digitMap['5'] = "jkl";
+        digitMap['6'] = "mno";
+        digitMap['7'] = "pqrs";
+        digitMap['8'] = "tuv";
+        digitMap['9'] = "wxyz";
+        
+        string currentCombination = "";
+        vector<string> result;
+        
+        helper(0, digits, currentCombination, digitMap, result);
+        return result;
     }
 };

65909f34ecddb36ccac779c5eb9093a70cebed72
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-08-14 15:49:47 +0530

    Added Bubble Sort

--- Sorting/Sorting Algorithms/bubble_sort.cpp
+++ Sorting/Sorting Algorithms/bubble_sort.cpp
@@ -6,32 +6,20 @@
 
 //Stable
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void bubbleSort(vector<int>& a) {
-    int n = a.size();
-    for (int i = 0; i < n - 1; ++i) {
-        bool swapped = false;
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (a[j] > a[j + 1]) {
-                swap(a[j], a[j + 1]);
-                swapped = true;    
+class Solution {
+public:
+    void bubbleSort(int arr[], int n) {
+        for (int i = n - 1; i >= 0; --i) {
+            bool swapped = false;
+            for (int j = 0; j < i; ++j) { 
+                if (arr[j] > arr[j + 1]) { 
+                    swap(arr[j], arr[j + 1]);
+                    swapped = true;
+                }
             }
+            
+            if (!swapped)
+                break;
         }
-        if (!swapped)
-            break;
     }
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
-    bubbleSort(a);
-    for (int i = 0; i < n; ++i)
-        cout << a[i] << " ";
-    return 0;
-}
+};

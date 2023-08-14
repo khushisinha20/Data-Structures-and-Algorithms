2c19df5da55d9ed273890eb9dfafc9e8f20f6c3c
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-08-14 16:05:25 +0530

    Added Insertion Sort

--- Sorting/Sorting Algorithms/insertion_sort.cpp
+++ Sorting/Sorting Algorithms/insertion_sort.cpp
@@ -7,26 +7,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insertionSort(vector<int>& a) {
-    for (int i = 1; i < a.size(); ++i) {
-        int key = a[i];
-        int j = i - 1;
-        while (j >= 0 && a[j] > key) {
-            a[j + 1] = a[j];
-            --j;
+class Solution {
+    public:
+    void insertionSort(int arr[], int n) {
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 and arr[j] > key) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = key;
         }
-        a[j + 1] = key;
     }
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
-    insertionSort(a);
-    for (int i = 0; i < a.size(); ++i)
-        cout << a[i] << " ";
-    return 0;
-}
+};

c5017470cf827b1f954b9f4515b8c8bb541a21a7
Khushi Sinha <khushisinha4786@gmail.com>
2023-10-16 11:40:04 +0530

    Added Find in Mountain Array

--- Searching/find_in_mountain_array.cpp
+++ Searching/find_in_mountain_array.cpp
@@ -14,74 +14,75 @@ using namespace std;
  */
 
 class MountainArray {
-    public:
-      int get(int index);
-      int length();
+  public:
+    int get(int index);
+    int length();
 };
 
 class Solution {
 public:
-    int findMaximum(MountainArray& mountainArr) {
-        int low = 0;
-        int n = mountainArr.length();
-        int high = n - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int mid_element = mountainArr.get(mid);
-            if (mid > 0 && mid < n - 1) {
-                if (mid_element > mountainArr.get(mid - 1) && mid_element > mountainArr.get(mid + 1))
-                    return mid;
-                else if (mid_element < mountainArr.get(mid - 1))
-                    high = mid - 1;
-                else 
-                    low = mid + 1;
-            } else if (mid == 0) {
-                if (mid_element > mountainArr.get(mid + 1))
-                    return 0;
-                return 1;
+    int findPeakIndex(MountainArray& mountainArr) {
+        int left = 0;
+        int right = mountainArr.length() - 1;
+        int answer = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = mountainArr.get(mid);
+            int midPlusOneValue = mountainArr.get(mid + 1);
+
+            if (midValue >= midPlusOneValue) {
+                answer = mid;
+                right = mid - 1;
             } else {
-                if (mid_element > mountainArr.get(n - 2))
-                    return n - 1;
-                return n - 2;
+                left = mid + 1;
             }
         }
+
+        return answer;
+    }
+    
+    int binarySearchAscending(MountainArray& mountainArr, int& target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = mountainArr.get(mid);
+            
+            if (midValue == target)
+                return mid;
+            else if (midValue < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
         return -1;
     }
     
-    int findBeforeMaximum(MountainArray& mountainArr, int target, int low, int high) {
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int mid_element = mountainArr.get(mid);
-            if (mid_element == target)
+    int binarySearchDescending(MountainArray& mountainArr, int& target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = mountainArr.get(mid);
+            
+            if (midValue == target)
                 return mid;
-            else if (mid_element < target)
-                low = mid + 1;
-            else 
-                high = mid - 1;
+            else if (midValue < target)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
-        return INT_MAX;
-    }
-    
-    int findAfterMaximum(MountainArray& mountainArr, int target, int low, int high) {
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int mid_element = mountainArr.get(mid);
-            if (mid_element == target)
-                return mid;
-            else if (mid_element < target)
-                high = mid - 1;
-            else 
-                low = mid + 1;
-        }
-        return INT_MAX;
+        
+        return -1;
     }
     
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        int maxInMountainArray = findMaximum(mountainArr);
-        int n = mountainArr.length();
-        int occurenceBeforeMaximum = findBeforeMaximum(mountainArr, target, 0, maxInMountainArray - 1);
-        int occurenceAfterMaximum = findAfterMaximum(mountainArr, target, maxInMountainArray, n - 1);
-        int res = min(occurenceBeforeMaximum, occurenceAfterMaximum);
-        return res == INT_MAX ? -1 : res;
+        int peakIndex = findPeakIndex(mountainArr);
+        
+        int leftResult = binarySearchAscending(mountainArr, target, 0, peakIndex);
+        
+        if (leftResult != -1)
+            return leftResult;
+        
+        int rightResult = binarySearchDescending(mountainArr, target, peakIndex + 1, mountainArr.length() - 1);
+        return rightResult;
     }
 };

d3edeb063f6a68f89f9df24e78f87c5cdc94980e
Khushi Sinha <khushisinha4786@gmail.com>
2023-10-16 11:42:34 +0530

    Added Max Chunks To Make Sorted

---
+++ Greedy/max_chunks_to_make_sorted.cpp
@@ -0,0 +1,20 @@
+//leetcode.com/problems/max-chunks-to-make-sorted/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int sum = 0;
+        int ans = 0;
+        
+        for (int i = 0; i < arr.size(); ++i) {
+            sum += arr[i];
+            if ((i * (i + 1)) / 2 == sum)
+                ++ans;
+        }
+        
+        return ans;
+    }
+};

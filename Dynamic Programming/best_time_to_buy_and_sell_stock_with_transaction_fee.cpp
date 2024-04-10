390fa0e9466d62ed3d15e77e38ee5886ecad7cef
Khushi Sinha <khushisinha4786@gmail.com>
2024-04-11 02:04:46 +0530

    Added Best Time to Buy and Sell Stock with Transaction Fee

--- Dynamic Programming/best_time_to_buy_and_sell_stock_with_transaction_fee.cpp
+++ Dynamic Programming/best_time_to_buy_and_sell_stock_with_transaction_fee.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
 class Solution {
 public:
     int helper(vector<vector<int>>& dp, int index, bool canBuy, vector<int>& prices, int& fee) {
@@ -36,5 +37,30 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(2, -1));
         return helper(dp, 0, true, prices, fee);
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int> (3, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int canBuy = 0; canBuy < 2; ++canBuy) {
+                if (canBuy) {
+                    int buy = -prices[i] + dp[i + 1][0];
+                    int notBuy = dp[i + 1][1];
+                    dp[i][canBuy] = max(buy, notBuy);
+                } else {
+                    int sell = prices[i] - fee + dp[i + 1][1];
+                    int notSell = dp[i + 1][0];
+                    dp[i][canBuy] = max(sell, notSell);
+                }
+            }
+        }
+
+        return dp[0][1];
     }
 };
